402a20500311665910378485ec2204d2b4d156f6
7Steffen7 <steffenparth@macbookpro.speedport.ip>
2024-12-22 17:03:25 +0100

    mariaDB somehow working

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,1 +1,28 @@
-echo "HelloWorld"
+#!/bin/bash
+
+# echo "HelloWorld"
+
+# startup mysq;
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+# service mysql start;
+exec mysqld_safe &
+
+until mysqladmin ping --silent; do
+	echo "Waiting for MariaDB to start..."
+	sleep 2
+done
+# create table
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
